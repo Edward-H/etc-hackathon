66ab3852d671fdd9d1d726d660ff071b76be0650
Edward Hart <edward.dan.hart@gmail.com>
2016-11-19 11:34:48 +0000

    Fixed syntax errors and run autopep8 on code.

--- main.py
+++ main.py
@@ -5,22 +5,30 @@ import sys
 import socket
 import json
 
-bank("money":0, "BOND":0, "VALBZ":0, "VALE":0, "GS":0, "MS":0, "WFC":0, "XLF":0)
-
-def add(id,stock,dir,price,size)
-    if(dir==true) b="BUY"
-    else b="SELL"
-    write(exchange,{"type": "add", "order_id": id, "symbol": stock, "dir": b, "price": price, "size": size})
-
-def convert(id,stock,dir,size)
-    if(dir==true) b="BUY"
-    else b="SELL"
-    write(exchange,{"type": "convert", "order_id": id, "symbol": stock, "dir": b, "size": size})
-
-def cancel(id)
-    write(exchange{"type": "cancel", "order_id": id})
+bank = {"money": 0, "BOND": 0, "VALBZ": 0, "VALE": 0, "GS": 0, "MS": 0, "WFC": 0, "XLF": 0}
 
 
+def add(id, stock, dir, price, size):
+    b = ""
+    if dir == true:
+        b = "BUY"
+    else:
+        b = "SELL"
+    write(exchange, {"type": "add", "order_id": id, "symbol":
+          stock, "dir": b, "price": price, "size": size})
+
+
+def convert(id, stock, dir, size):
+    if dir == true:
+        b = "BUY"
+    else:
+        b = "SELL"
+    write(exchange,
+          {"type": "convert", "order_id": id, "symbol": stock, "dir": b, "size": size})
+
+
+def cancel(id):
+    write(exchange, {"type": "cancel", "order_id": id})
 
 import pandas as pd
 
@@ -29,14 +37,16 @@ import pandas as pd
 # XLF: 3 BOND 2 GS 3 MS 2 WFC
 
 symbols = ["BOND", "VALBZ", "VALE", "GS", "MS", "WFC", "XLF"]
-price = pd.DataFrame(columns=symbols) # Trading Price of transation
-volume = pd.DataFrame(columns=symbols) # Trading Volume of transaction
+price = pd.DataFrame(columns=symbols)  # Trading Price of transation
+volume = pd.DataFrame(columns=symbols)  # Trading Volume of transaction
 books = {}
+
 
 def parse(message):
     if message["type"] == "trade":
         price.loc[price.shape[0], message["symbol"]] = float(message["price"])
         volume.loc[price.shape[0], message["symbol"]] = float(message["size"])
+
 
 def backfill_data():
     price.fillna(method="ffill", inplace=True)
@@ -44,9 +54,10 @@ def backfill_data():
     volume.fillna(method="ffill", inplace=True)
     volume.fillna(method="bfill", inplace=True)
 
+
 def get_latest_price():
     backfill_data()
-    latest_price = price.ix[price.shape[0]-1].to_dict()
+    latest_price = price.ix[price.shape[0] - 1].to_dict()
     return latest_price
 
 
@@ -55,12 +66,15 @@ def connect():
     s.connect(("test-exch-carrot", 25000))
     return s.makefile('rw', 1)
 
+
 def write(exchange, obj):
     json.dump(obj, exchange)
     exchange.write("\n")
 
+
 def read(exchange):
     return json.loads(exchange.readline())
+
 
 def main():
     exchange = connect()
