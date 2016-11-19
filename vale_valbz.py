0fbac47bf3a652061000d38762e15eb2d8762144
Edward Hart <edward.dan.hart@gmail.com>
2016-11-19 16:15:04 +0000

    Fixed syntax error.

--- vale_valbz.py
+++ vale_valbz.py
@@ -13,7 +13,7 @@ def trade_vale_valbz(id):
             exchange, {"type": "add", "order_id": id + 1, "symbol": "VALE", "dir": "SELL", "price": buy1, "size": 1})
         write(
             exchange, {"type": "convert", "order_id": id + 2, "symbol": "VALE", "dir": "BUY", "size": 1})
-    else if(buy2 - sell1 >= 20 and buy2 - sell1 < 100):
+    elif(buy2 - sell1 >= 20 and buy2 - sell1 < 100):
         write(
             exchange, {"type": "add", "order_id": id, "symbol": "VALBZ", "dir": "SELL", "price": buy2, "size": 1})
         write(
