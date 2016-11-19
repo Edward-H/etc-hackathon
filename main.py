e4b5ff74016c4720032b9daebe8223ff30d50c1d
Edward Hart <edward.dan.hart@gmail.com>
2016-11-19 11:59:11 +0000

    Added draft bond trading strategy.

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from parse_public_message import *
 
 bank = {"money": 0, "BOND": 0, "VALBZ": 0, "VALE": 0, "GS": 0, "MS": 0, "WFC": 0, "XLF": 0}
 
+id = 0
 
 def add(id, stock, dir, price, size):
-    b = ""
     if dir == true:
         b = "BUY"
     else:
@@ -27,9 +27,6 @@ def convert(id, stock, dir, size):
         b = "SELL"
     write(exchange,
           {"type": "convert", "order_id": id, "symbol": stock, "dir": b, "size": size})
-
-
-
 
 def cancel(id):
     write(exchange, {"type": "cancel", "order_id": id})
@@ -63,8 +60,6 @@ def get_latest_price():
     backfill_data()
     latest_price = price.ix[price.shape[0] - 1].to_dict()
     return latest_price
->>>>>>> origin/master
-
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -80,12 +75,27 @@ def write(exchange, obj):
 def read(exchange):
     return json.loads(exchange.readline())
 
+def update_bond_holdings():
+    # TO-DO: Cancel orders when impossible/better options exist.
+    current_bond_price = get_latest_price()["BOND"]
+    if current_bond_price > 1000 and bank["BOND"] > 0:
+        # Sell bonds (if we have any) if they are more than 1000.
+        id += 1
+        add(id, "BOND", false, current_bond_price + 1, min(bank["BOND"], entry.size))
+    elif current_bond_price < 1000 and bank["BOND"] < 100:
+        # Buy more bonds (if we can) if they are less than 1000.
+        id += 1
+        add(id, "BOND", true, current_bond_price - 1, 100 - bank["BOND"])
 
 def main():
     exchange = connect()
     write(exchange, {"type": "hello", "team": "CARROT"})
-    hello_from_exchange = read(exchange)
-    print("The exchange replied:", hello_from_exchange, file=sys.stderr)
+    while true:
+        exchange_messages = read(exchange)
+        for msg in exchange_messages:
+            parse(msg)
+        update_bond_holdings()
 
 if __name__ == "__main__":
     main()
+
