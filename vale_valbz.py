5aab9f559c1c77c49ed5f1954a40b655239a4dbd
Gelu <gelu.vrabie@univ.ox.ac.uk>
2016-11-19 17:29:43 +0000

    .

--- vale_valbz.py
+++ vale_valbz.py
@@ -6,14 +6,14 @@ def trade_vale_valbz(id):
     (buy2, sell2) = get_books_mbms("VALBZ")
     if((not buy1) or (not sell1) or (not buy2) or (not sell2)):
         return
-    if(buy1 - sell2 >= 15 and buy1 - sell2 < 100):
+    if(buy1 - sell2 >= 20 and buy1 - sell2 < 100):
         write(exchange, {"type": "add", "order_id": id, "symbol":
               "VALBZ", "dir": "BUY", "price": sell2, "size": 1})
         write(
             exchange, {"type": "add", "order_id": id + 1, "symbol": "VALE", "dir": "SELL", "price": buy1, "size": 1})
         write(
             exchange, {"type": "convert", "order_id": id + 2, "symbol": "VALE", "dir": "BUY", "size": 1})
-    elif(buy2 - sell1 >= 15 and buy2 - sell1 < 100):
+    elif(buy2 - sell1 >= 20 and buy2 - sell1 < 100):
         write(
             exchange, {"type": "add", "order_id": id, "symbol": "VALBZ", "dir": "SELL", "price": buy2, "size": 1})
         write(
