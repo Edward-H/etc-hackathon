6e6b27265970dc14eebe2e45afcb6b323dd86438
Gelu <gelu.vrabie@univ.ox.ac.uk>
2016-11-19 16:29:11 +0000

    fix

--- main.py
+++ main.py
@@ -147,6 +147,26 @@ def trade_stock(stock):
         if(bank[stock] + pending_bank[stock] >= -90):
             unverified_orders.append(order)
 
+def trade_vale_valbz(id):
+    (buy1, sell1) = get_books_mbms("VALE")
+    (buy2, sell2) = get_books_mbms("VALBZ")
+    if((not buy1) or (not sell1) or (not buy2) or (not sell2)):
+        return
+    if(buy1 - sell2 >= 15 and buy1 - sell2 < 100):
+        write(exchange, {"type": "add", "order_id": id, "symbol":
+              "VALBZ", "dir": "BUY", "price": sell2, "size": 1})
+              write(
+                    exchange, {"type": "add", "order_id": id + 1, "symbol": "VALE", "dir": "SELL", "price": buy1, "size": 1})
+              write(
+                    exchange, {"type": "convert", "order_id": id + 2, "symbol": "VALE", "dir": "BUY", "size": 1})
+    elif(buy2 - sell1 >= 15 and buy2 - sell1 < 100):
+        write(
+              exchange, {"type": "add", "order_id": id, "symbol": "VALBZ", "dir": "SELL", "price": buy2, "size": 1})
+              write(
+                    exchange, {"type": "add", "order_id": id + 1, "symbol": "VALE", "dir": "BUY", "price": sell1, "size": 1})
+              write(
+                    exchange, {"type": "convert", "order_id": id + 2, "symbol": "VALE", "dir": "SELL", "size": 1})
+
 
 def main():
     global exchange
