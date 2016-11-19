0f215c6d3bf64cef01ab4c9c4ed9d4f1cfe163ce
chunngaiau <chunngaiau@gmail.com>
2016-11-19 12:01:11 +0000

    a

--- main.py
+++ main.py
@@ -34,37 +34,6 @@ def convert(id, stock, dir, size):
 def cancel(id):
     write(exchange, {"type": "cancel", "order_id": id})
 
-import pandas as pd
-
-# Symbols
-# BOND, VALBZ, VALE, GS, MS, WFC, XLF
-# XLF: 3 BOND 2 GS 3 MS 2 WFC
-
-symbols = ["BOND", "VALBZ", "VALE", "GS", "MS", "WFC", "XLF"]
-price = pd.DataFrame(columns=symbols)  # Trading Price of transation
-volume = pd.DataFrame(columns=symbols)  # Trading Volume of transaction
-books = {}
-
-
-def parse(message):
-    if message["type"] == "trade":
-        price.loc[price.shape[0], message["symbol"]] = float(message["price"])
-        volume.loc[price.shape[0], message["symbol"]] = float(message["size"])
-
-
-def backfill_data():
-    price.fillna(method="ffill", inplace=True)
-    price.fillna(method="bfill", inplace=True)
-    volume.fillna(method="ffill", inplace=True)
-    volume.fillna(method="bfill", inplace=True)
-
-
-def get_latest_price():
-    backfill_data()
-    latest_price = price.ix[price.shape[0] - 1].to_dict()
-    return latest_price
->>>>>>> origin/master
-
 
 def connect():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
