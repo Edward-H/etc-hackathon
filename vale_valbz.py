e62716ca0e8acf0a83fcdee9339d7037954f9420
Gelu <gelu.vrabie@univ.ox.ac.uk>
2016-11-19 15:58:40 +0000

    .

--- vale_valbz.py
+++ vale_valbz.py
@@ -8,7 +8,7 @@ def trade_vale_valbz(id):
         write(exchange,{"type": "add", "order_id": id, "symbol": "VALBZ", "dir": "BUY", "price": sell2, "size": 1})
         write(exchange,{"type": "add", "order_id": id+1, "symbol": "VALE", "dir": "SELL", "price": buy1, "size": 1})
         write(exchange,{"type": "convert", "order_id": id+2, "symbol": "VALE", "dir": "BUY", "size": 1})
-    else if(buy2-sell1>=20)
+    else if(buy2-sell1>=20):
         write(exchange,{"type": "add", "order_id": id, "symbol": "VALBZ", "dir": "SELL", "price": buy2, "size": 1})
         write(exchange,{"type": "add", "order_id": id+1, "symbol": "VALE", "dir": "BUY", "price": sell1, "size": 1})
         write(exchange,{"type": "convert", "order_id": id+2, "symbol": "VALE", "dir": "SELL", "size": 1})
