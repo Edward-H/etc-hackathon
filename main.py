1ea4df53d0531ea231e621e768745c7c89e06322
Gelu <gelu.vrabie@univ.ox.ac.uk>
2016-11-19 17:56:13 +0000

    fix

--- main.py
+++ main.py
@@ -154,18 +154,18 @@ def trade_vale_valbz(id):
     (buy2, sell2) = get_books_mbms("VALBZ")
     if((not buy1) or (not sell1) or (not buy2) or (not sell2)):
         return
-    if(buy1 - sell2 >= 20 and buy1 - sell2 < 100):
+    if(buy1 - sell2 >= 15 and buy1 - sell2 < 100):
         write(exchange, {"type": "add", "order_id": id, "symbol":
-              "VALBZ", "dir": "BUY", "price": sell2+2, "size": 1})
+              "VALBZ", "dir": "BUY", "price": sell2, "size": 1})
         write(
-            exchange, {"type": "add", "order_id": id + 1, "symbol": "VALE", "dir": "SELL", "price": buy1-2, "size": 1})
+            exchange, {"type": "add", "order_id": id + 1, "symbol": "VALE", "dir": "SELL", "price": buy1, "size": 1})
         write(
             exchange, {"type": "convert", "order_id": id + 2, "symbol": "VALE", "dir": "BUY", "size": 1})
-    elif(buy2 - sell1 >= 20 and buy2 - sell1 < 100):
+    elif(buy2 - sell1 >= 15 and buy2 - sell1 < 100):
         write(
-            exchange, {"type": "add", "order_id": id, "symbol": "VALBZ", "dir": "SELL", "price": buy2-2, "size": 1})
+            exchange, {"type": "add", "order_id": id, "symbol": "VALBZ", "dir": "SELL", "price": buy2, "size": 1})
         write(
-            exchange, {"type": "add", "order_id": id + 1, "symbol": "VALE", "dir": "BUY", "price": sell1+2, "size": 1})
+            exchange, {"type": "add", "order_id": id + 1, "symbol": "VALE", "dir": "BUY", "price": sell1, "size": 1})
         write(
             exchange, {"type": "convert", "order_id": id + 2, "symbol": "VALE", "dir": "SELL", "size": 1})
 
@@ -191,7 +191,6 @@ def main():
             for order in pending_orders:
                 print(order)
             print("\n")
-            time.sleep(0.1)
     except KeyboardInterrupt:
         exchange.close()
         print(bank)
